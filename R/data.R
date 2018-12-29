ac8e38d89824f1ed37d173defa8474a17512e7be
Catherine Blatter <catherine.blatter@unibas.ch>
2018-12-29 08:01:11 +0100

    documented code

--- R/data.R
+++ R/data.R
@@ -1,9 +1,8 @@
-#'
 #' Diabetes
 #'
-#'
+#' A fake dataframe with variables somewhat related to Diabetes.
+#' Contains character and numeric variables including NAs.
 #'
 #' @format A sample data frame containing data from 130 probands in 11 variables
 #'
 #'
-Diabetes
