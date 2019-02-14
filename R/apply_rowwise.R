6b92f01937389546d35c3ec9930c86742eb181a2
Catherine Blatter <catherine.blatter@unibas.ch>
2019-02-14 09:38:59 +0100

    added apply_rowwise-func

---
+++ R/apply_rowwise.R
@@ -0,0 +1,17 @@
+
+#' Apply a function rowwise (usually per observation)
+#'
+#' @param data a dataframe
+#' @param vars a selection of variables from the dataframe to apply a function over
+#' @param na.rm TRUE or FALSE, default = TRUE
+#' @param func the function to apply
+#'
+#' @return
+#' @export
+#'
+#' @examples
+apply_rowwise <- function(data, vars, func, na.rm = TRUE){
+
+  apply(data[ , vars], 1, func, na.rm = na.rm)
+
+}
